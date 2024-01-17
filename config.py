f28fe379ca41a64891b09242ed412491f6058b3f
Cookiext23 <Cookie.Xie@lincolnuni.ac.nz>
2024-01-17 23:15:15 +1300

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+# Change the values below based on your MySQL setup
+
 SECRET_KEY = "dev"
 DB_USER = "root"
-DB_PASS = "19890213rong"
+DB_PASS = "root"
 DB_HOST = "localhost"
 DB_PORT = "3306"
 DB_NAME = "gym"
